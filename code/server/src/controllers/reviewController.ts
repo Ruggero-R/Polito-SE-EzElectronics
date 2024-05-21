2039284ca1eb2272a1a2903e4890b3075c839e1b
RUGGERO ROSSINO s323698 <s323698@studenti.polito.it>
2024-05-21 10:49:43 +0200

    add: implementati i metodi di ReviewController

--- code/server/src/controllers/reviewController.ts
+++ code/server/src/controllers/reviewController.ts
@@ -16,14 +16,18 @@ class ReviewController {
      * @param comment The comment made by the user
      * @returns A Promise that resolves to nothing
      */
-    async addReview(model: string, user: User, score: number, comment: string) /**:Promise<void> */ { }
+    async addReview(model: string, user: User, score: number, comment: string) /**:Promise<void> */ {
+        return this.dao.addReview(model, user, score, comment);
+    }
 
     /**
      * Returns all reviews for a product
      * @param model The model of the product to get reviews from
      * @returns A Promise that resolves to an array of ProductReview objects
      */
-    async getProductReviews(model: string) /**:Promise<ProductReview[]> */ { }
+    async getProductReviews(model: string) /**:Promise<ProductReview[]> */ { 
+        return this.dao.getProductReviews(model);
+    }
 
     /**
      * Deletes the review made by a user for a product
@@ -31,20 +35,26 @@ class ReviewController {
      * @param user The user who made the review to delete
      * @returns A Promise that resolves to nothing
      */
-    async deleteReview(model: string, user: User) /**:Promise<void> */ { }
+    async deleteReview(model: string, user: User) /**:Promise<void> */ { 
+        return this.dao.deleteReview(model, user);
+    }
 
     /**
      * Deletes all reviews for a product
      * @param model The model of the product to delete the reviews from
      * @returns A Promise that resolves to nothing
      */
-    async deleteReviewsOfProduct(model: string) /**:Promise<void> */ { }
+    async deleteReviewsOfProduct(model: string) /**:Promise<void> */ { 
+        return this.dao.deleteReviewsOfProduct(model);
+    }
 
     /**
      * Deletes all reviews of all products
      * @returns A Promise that resolves to nothing
      */
-    async deleteAllReviews() /**:Promise<void> */ { }
+    async deleteAllReviews() /**:Promise<void> */ { 
+        return this.dao.deleteAllReviews();
+    }
 }
 
 export default ReviewController;
--- code/server/src/dao/reviewDAO.ts
+++ code/server/src/dao/reviewDAO.ts
@@ -1,3 +1,11 @@
+import db from "../db/db"
+import { resolve } from "path";
+import { User } from "../components/user";
+import { ProductReview } from "../components/review";
+import { rejects } from "assert";
+import dayjs from "dayjs";
+import { ExistingReviewError, NoReviewProductError } from "../errors/reviewError";
+import { Product } from "../components/product";
 
 /**
  * A class that implements the interaction with the database for all review-related operations.
@@ -5,6 +13,130 @@
  */
 class ReviewDAO {
 
+    addReview(model: string, user: User, score: number, comment: string): Promise<void> {
+        // DA MODIFICARE UNA VOLTA CHE IL DAO DEI PRODOTTI SARÀ COMPLETATO, usare getProducts invece della query sql
+
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const sql = "SELECT * FROM products WHERE model = ?";
+                db.get(sql, [model], (err: Error | null, row: Product) => {
+                    if(err) {
+                        reject(err);
+                    } else if (!row) {
+                        reject(new NoReviewProductError);
+                    } else {
+                        const sql = "INSERT INTO products_reviews(model, user, score, date, comment) VALUES(?, ?, ?, ?, ?)";
+                        db.run(sql, [model, user, score, dayjs().format("YYYY-MM-DD"), comment], (err: Error | null) => {
+                            if (err) {
+                                if (err.message.includes("FOREIGN KEY constraint failed"))
+                                    reject(new ExistingReviewError);
+                                reject(err);
+                            }
+                            resolve();
+                        });
+                    }
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    getProductReviews(model: string): Promise<ProductReview[]> {
+        // DA MODIFICARE UNA VOLTA CHE IL DAO DEI PRODOTTI SARÀ COMPLETATO, usare getProducts invece della query sql
+
+        return new Promise<ProductReview[]>((resolve, reject) => {
+            try {
+                const sql = "SELECT * FROM products WHERE model = ?";
+                db.get(sql, [model], (err: Error | null, row: Product) => {
+                    if(err) {
+                        reject(err);
+                    } else if (!row) {
+                        reject(new NoReviewProductError);
+                    } else {
+                        const sql = "SELECT * FROM products_reviews WHERE model = ?";
+                        db.all(sql, [model], (err: Error | null, rows: ProductReview[]) => {
+                            if (err) {
+                                reject(err);
+                            }
+                            resolve(rows);
+                        });
+                    }
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    deleteReview(model: string, user: User): Promise<void> {
+        // DA MODIFICARE UNA VOLTA CHE IL DAO DEI PRODOTTI SARÀ COMPLETATO, usare getProducts invece della query sql
+
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const sql = "SELECT * FROM products WHERE model = ?";
+                db.get(sql, [model], (err: Error | null, row: Product) => {
+                    if (err) {
+                        reject(err);
+                    } else if (!row) {
+                        reject(new NoReviewProductError);
+                    } else {
+                        const sql = "DELETE FROM products_reviews WHERE model = ? AND user = ?";
+                        db.run(sql, [model, user], (err: Error | null) => {
+                            if (err) {
+                                if (err.message.includes("FOREIGN KEY constraint failed"))
+                                    reject(new ExistingReviewError);
+                            }
+                            resolve();
+                        });
+                    }
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    deleteReviewsOfProduct(model: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const sql = "SELECT * FROM products WHERE model = ?";
+                db.get(sql, [model], (err: Error | null, row: Product) => {
+                    if (err) {
+                        reject(err);
+                    } else if (!row) {
+                        reject(new NoReviewProductError);
+                    } else {
+                        const sql = "DELETE FROM products_reviews WHERE model = ?";
+                        db.run(sql, [model], (err: Error | null) => {
+                            if (err) {
+                                reject(err);
+                            }
+                            resolve();
+                        });
+                    }
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    deleteAllReviews(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const sql = "DELETE FROM products_reviews";
+                db.run(sql, [], (err: Error | null) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 }
 
 export default ReviewDAO;
