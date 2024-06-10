726d2bda56b7264ba8d388c558aab9a1555f5f81
VALERIO COPPOLA NERI s327229 <s327229@studenti.polito.it>
2024-06-11 01:46:30 +0200

    DAO_db; fix

--- code/server/test_integration/test_cart/DAO_db_integration.test.ts
+++ code/server/test_integration/test_cart/DAO_db_integration.test.ts
@@ -1,6 +1,9 @@
 import CartDAO from '../../src/dao/cartDAO';
 import { expect, beforeEach, describe, test, afterAll } from '@jest/globals';
 import db from '../../src/db/db';
+import { CartNotFoundError, ProductInCartError, ProductNotInCartError, WrongUserCartError, EmptyCartError } from "../../src/errors/cartError";
+import { EmptyProductStockError, LowProductStockError, ProductNotFoundError } from "../../src/errors/productError";
+
 
 describe('CartDAO integration tests', () => {
     let dao: CartDAO;
@@ -12,9 +15,103 @@ describe('CartDAO integration tests', () => {
                 if (err) {
                     console.log(err);
                 }
-                done();
+                db.run('DELETE FROM carts_items', (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    done();
+                });
             });
         });
+
     });
+
+    /* ****************************************** *
+     * Integration test for the createCart method *    
+     * ****************************************** */
+    test('createCart should create a new cart for a customer', async () => {
+        await dao.createCart('customer1');
+        const carts = await dao.getActiveCartByUserId('customer1');
+        expect(carts).toBeTruthy();
+        expect(carts.customer).toBe('customer1');
+        expect(carts.paid).toBe(false);
+        expect(carts.products.length).toBe(0);
+    });
+
+    /* ************************************************ *
+     * Integration test for the addProductToCart method *    
+     * ************************************************ */
+
+
+
+    /* ***************************************************** *
+     * Integration test for the getActiveCartByUserId method *    
+     * ***************************************************** */
+
+
+
+    /* ************************************************* *
+     * Integration test for the userHasActiveCart method *    
+     * ************************************************* */
+
+
+
+    /* ********************************************** *
+     * Integration test for the updateCartItem method *    
+     * ********************************************** */
+
+
+
+    /* *********************************************** *
+     * Integration test for the updateCartTotal method *    
+     * *********************************************** */
+
+
+
+    /* ******************************************** *
+     * Integration test for the checkoutCart method *    
+     * ******************************************** */
+
+    test('checkoutCart should throw EmptyCartError if the cart is empty', async () => {
+        await dao.createCart('customer1');
+        await expect(dao.checkoutCart('customer1')).rejects.toThrow(EmptyCartError);
+    });
+
+
+    /* ***************************************************** *
+     * Integration test for the removeProductFromCart method *        //COME GESTIRE L'ERRORE? ProductNotInCartError o ProductNotFoundError
+     * ***************************************************** */
+
+    test('removeProductFromCart should throw ProductNotInCartError if product is not in the cart', async () => {
+        await dao.createCart('customer1');
+        await expect(dao.removeProductFromCart('customer1', 'prod1')).rejects.toThrow(ProductNotInCartError);
+    });
+
+
+    /* ***************************************** *
+     * Integration test for the clearCart method *    
+     * ***************************************** */
+
+
+
+    /* ********************************************** *
+     * Integration test for the deleteAllCarts method *    
+     * ********************************************** */
+
+
+
+
+    /* ******************************************* *
+     * Integration test for the getAllCarts method *    
+     * ******************************************* */
+
+
+
+
+    /* ************************************************ *
+     * Integration test for the getCustomerCarts method *    
+     * ************************************************ */
+
+
 
 });
