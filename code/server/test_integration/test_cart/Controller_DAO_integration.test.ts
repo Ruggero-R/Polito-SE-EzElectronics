71138e3ed9c7800c461c7ab0c003e6c37366fb11
Mattia Carlino s324218 <s324218@studenti.polito.it>
2024-06-14 11:58:32 +0200

    add: dao_db_cart integration test

--- code/server/test_integration/test_cart/Controller_DAO_integration.test.ts
+++ code/server/test_integration/test_cart/Controller_DAO_integration.test.ts
@@ -1,23 +1,207 @@
-import CartController from '../../src/controllers/cartController';
-import { expect, beforeEach, describe, test } from '@jest/globals';
-import db from '../../src/db/db';
+import { test, expect, jest, describe, afterEach, beforeAll, afterAll} from "@jest/globals";
+import CartController from "../../src/controllers/cartController";
+import CartDAO from "../../src/dao/cartDAO";
+import ProductDAO from "../../src/dao/productDAO";
 
-describe('Cart Controller Integration Tests', () => {
-    let controller: CartController;
-
-    beforeEach((done) => {
-        controller = new CartController();
-        db.serialize(() => {
-            db.run('DELETE FROM carts', (err) => {
-                if (err) {
-                    console.log(err);
-                }
-                done();
-            });
-        });
-    });
+import { Category } from "../../src/components/product";
+import { Role, User } from "../../src/components/user";
+import { InvalidParametersError } from "../../src/errors/cartError";
 
 
+let cartDAO = new CartDAO();
 
+const mockCarts = [
+  {
+    customer: "user1",
+    paid: false,
+    paymentDate: null,
+    total: 0,
+    products: [
+    ],
+  },
+  {
+    customer: "user2",
+    paid: true,
+    paymentDate: "2022-01-01",
+    total: 50,
+    products: [
+      {
+        model: "model3",
+        quantity: 1,
+        category: Category.SMARTPHONE,
+        price: 50,
+      },
+    ],
+  },
+];
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
+
+const Controller = new CartController();
+const Utente1 = new User(
+  "user1",
+  "Mattia",
+  "Carlino",
+  Role.ADMIN,
+  "matto@polito.it",
+  "2024-01-01"
+);
+const Utente2 = new User(
+  " ",
+  "Mattia",
+  "Carlino",
+  Role.ADMIN,
+  "matto@polito.it",
+  "2024-01-01"
+);
+
+beforeAll(async () => {
+    await ProductDAO.prototype.registerProducts("model1", Category.SMARTPHONE, 2, null, 10.0, "2024-01-01");
+})
+
+afterAll(async () => {
+    await ProductDAO.prototype.deleteAllProducts()
+});
+
+
+afterEach(async () => {
+    await cartDAO.deleteAllCarts();
+});
+
+/* ********************************************** *
+ *    Unit test for the addToCart method    *
+ * ********************************************** */
+
+describe("Unit Tests for the addToCart method", () => {
+  test("It should resolve", async () => {
+    await expect(Controller.addToCart(Utente1, "model1")).resolves.toBe(true);
+  });
+  test("It should reject due a model not given", () => {
+    expect(() => Controller.addToCart(Utente1, " ")).rejects.toThrow(
+      InvalidParametersError
+    );
+  });
+  test("It should reject due to a user not given", () => {
+    expect(() => Controller.addToCart(Utente2, "model1")).rejects.toThrow(
+      InvalidParametersError
+    );
+  });
+});
+
+
+/* ********************************************** *
+ *    Unit test for the getCart method    *
+ * ********************************************** */
+describe("Unit Tests for the getCart method", () => {
+  test("It should resolve", async () => {
+    await cartDAO.addProductToCart(Utente1.username, "model1");
+    expect(Controller.getCart(Utente1)).resolves.toEqual(mockCarts[0]);
+  });
+  test("It should reject due to a user not given", () => {
+    expect(() => Controller.getCart(Utente2)).rejects.toThrow(
+      InvalidParametersError
+    );
+  });
+});
+
+/* ********************************************** *
+ *    Unit test for the checkoutCart method    *
+ * ********************************************** */
+describe("Unit Tests for the checkoutCart method", () => {
+  test("It should resolve", async () => {
+    await cartDAO.addProductToCart(Utente1.username, "model1");
+    expect(Controller.checkoutCart(Utente1)).resolves.toBe(true);
+  });
+  test("It should reject due to a user not given", () => {
+    expect(() => Controller.checkoutCart(Utente2)).rejects.toThrow(
+      InvalidParametersError
+    );
+  });
+});
+
+// /* ********************************************** *
+//  *    Unit test for the getCustomerCarts method    *
+//  * ********************************************** */
+// describe("Unit Tests for the getCustomerCarts method", () => {
+//   test("It should resolve", () => {
+//     const mock = jest
+//       .spyOn(CartDAO.prototype, "getCustomerCarts")
+//       .mockResolvedValue([mockCarts[1]]);
+//     expect(Controller.getCustomerCarts(Utente1)).resolves.toEqual([
+//       mockCarts[1],
+//     ]);
+//   });
+//   test("It should reject due to a user not given", () => {
+//     expect(() => Controller.getCustomerCarts(Utente2)).rejects.toThrow(
+//       InvalidParametersError
+//     );
+//   });
+// });
+
+// /* ********************************************** *
+//  *    Unit test for the removeProductFromCart method    *
+//  * ********************************************** */
+// describe("Unit Tests for the removeProductFromCart method", () => {
+//   test("It should resolve", () => {
+//     const mock = jest
+//       .spyOn(CartDAO.prototype, "removeProductFromCart")
+//       .mockResolvedValue(true);
+//     expect(Controller.removeProductFromCart(Utente1, "model1")).resolves.toBe(
+//       true
+//     );
+//   });
+//   test("It should reject due a model not given", () => {
+//     expect(() => Controller.removeProductFromCart(Utente1, "")).rejects.toThrow(
+//       InvalidParametersError
+//     );
+//   });
+//   test("It should reject due to a user not given", () => {
+//     expect(() =>
+//       Controller.removeProductFromCart(Utente2, "model1")
+//     ).rejects.toThrow(InvalidParametersError);
+//   });
+// });
+
+// /* ********************************************** *
+//  *    Unit test for the clearCart method    *
+//  * ********************************************** */
+// describe("Unit Tests for the clearCart method", () => {
+//   test("It should resolve", () => {
+//     const mock = jest
+//       .spyOn(CartDAO.prototype, "clearCart")
+//       .mockResolvedValue(true);
+//     expect(Controller.clearCart(Utente1)).resolves.toBe(true);
+//   });
+//   test("It should reject due to a user not given", () => {
+//     expect(() => Controller.clearCart(Utente2)).rejects.toThrow(
+//       InvalidParametersError
+//     );
+//   });
+// });
+
+// /* ********************************************** *
+//  *    Unit test for the deleteAllCarts method    *
+//  * ********************************************** */
+// describe("Unit Tests for the deleteAllCarts method", () => {
+//   test("It should resolve", () => {
+//     const mock = jest
+//       .spyOn(CartDAO.prototype, "deleteAllCarts")
+//       .mockResolvedValue(true);
+//     expect(Controller.deleteAllCarts()).resolves.toBe(true);
+//   });
+// });
+
+// /* ********************************************** *
+//  *    Unit test for the getAllCarts method    *
+//  * ********************************************** */
+// describe("Unit Tests for the getAllCarts method", () => {
+//   test("It should resolve", () => {
+//     const mock = jest
+//       .spyOn(CartDAO.prototype, "getAllCarts")
+//       .mockResolvedValue(mockCarts);
+//     expect(Controller.getAllCarts()).resolves.toEqual(mockCarts);
+//   });
+// });
--- code/server/test_integration/test_cart/DAO_db_integration.test.ts
+++ code/server/test_integration/test_cart/DAO_db_integration.test.ts
@@ -1,117 +1,353 @@
-import CartDAO from '../../src/dao/cartDAO';
-import { expect, beforeEach, describe, test, afterAll } from '@jest/globals';
-import db from '../../src/db/db';
-import { CartNotFoundError, ProductInCartError, ProductNotInCartError, WrongUserCartError, EmptyCartError } from "../../src/errors/cartError";
-import { EmptyProductStockError, LowProductStockError, ProductNotFoundError } from "../../src/errors/productError";
+import { test, expect, jest, beforeEach, describe } from "@jest/globals";
+import CartDAO from "../../src/dao/cartDAO";
+import db from "../../src/db/db";
+import { Database } from "sqlite3";
+import { Cart, ProductInCart } from "../../src/components/cart";
+import {
+    CartNotFoundError,
+    ProductInCartError,
+    ProductNotInCartError,
+    WrongUserCartError,
+    EmptyCartError,
+    InvalidParametersError,
+    LowProductStockError,
+    NoCartItemsError,
+    AlreadyActiveCart,
+} from "../../src/errors/cartError";
+import { Category } from "../../src/components/product";
+import {
+    EmptyProductStockError,
+    ProductNotFoundError,
+} from "../../src/errors/productError";
+import { afterEach, beforeAll, afterAll } from "@jest/globals";
+import ProductDAO from "../../src/dao/productDAO";
+import dayjs from "dayjs";
 
 
-describe('CartDAO integration tests', () => {
-    let dao: CartDAO;
+let cartDAO = new CartDAO();
 
-    beforeEach((done) => {
-        dao = new CartDAO();
-        db.serialize(() => {
-            db.run('DELETE FROM carts', (err) => {
-                if (err) {
-                    console.log(err);
-                }
-                db.run('DELETE FROM carts_items', (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    done();
-                });
-            });
+beforeAll(async () => {
+    await ProductDAO.prototype.registerProducts("product1", Category.SMARTPHONE, 2, null, 10.0, "2024-01-01");
+})
+
+afterAll(async () => {
+    await ProductDAO.prototype.deleteAllProducts()
+});
+
+
+afterEach(async () => {
+    await cartDAO.deleteAllCarts();
+});
+
+describe("Unit test for the cartDAO class", () => {
+    /* ********************************************** *
+     *    Unit test for the createCart method    *
+     * ********************************************** */
+    describe("Unit test for the createCart method", () => {
+        test("It should create a cart", async () => {
+            await expect(cartDAO.createCart("customer")).resolves.toBeUndefined();
+        });
+
+
+        test("It should throw an error if a cart for the user already exists", async () => {
+            await cartDAO.createCart("customer")
+            await expect(cartDAO.createCart("customer")).rejects.toThrow(
+                AlreadyActiveCart
+            );
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the getActiveCartByUserId method    *
+     * ********************************************** */
+    describe("Unit test for the getActiveCartByUserId method", () => {
+        test("It should return a cart with items if the cart exists and has items", async () => {
+            await cartDAO.createCart("customer");
+
+            await cartDAO.addProductToCart("customer", "product1");
+
+            const result = await cartDAO.getActiveCartByUserId("customer");
+
+            expect(result).toEqual(
+                new Cart("customer", false, null as any, 10.0, [
+                    new ProductInCart("product1", 1, Category.SMARTPHONE, 10.0)
+                ])
+            );
+        });
+
+
+        test("It should return a cart without items if the cart exists but has no items", async () => {
+            await cartDAO.createCart("customer");
+            const result = await cartDAO.getActiveCartByUserId("customer");
+            expect(result).toEqual(new Cart("customer", false, null as any, 0.0, []));
+        });
+
+        test("It should return an empty cart if the cart does not exist", async () => {
+            const result = await cartDAO.getActiveCartByUserId("customer");
+            expect(result).toEqual(new Cart("customer", false, null as any, 0.0, []));
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the userHasActiveCart method  *
+     * ********************************************** */
+    describe("Unit test for the userHasActiveCart method", () => {
+        test("It should return true if the user has an active cart", async () => {
+            await cartDAO.createCart("customer");
+            const result = await cartDAO.userHasActiveCart("customer");
+            expect(result).toBe(true);
+        });
+
+        test("It should return false if the user does not have an active cart", async () => {
+            const result = await cartDAO.userHasActiveCart("customer");
+            expect(result).toBe(false);
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the addProductToCart method   *
+     * ********************************************** */
+    describe("Unit test for the addProductToCart method", () => {
+        test("It should add a new product to the exisisting cart of the user", async () => {
+            await cartDAO.createCart("customer");;
+            await expect(
+                cartDAO.addProductToCart("customer", "product1")
+            ).resolves.toBe(true);
+        });
+
+        test("It should add a new product to a new cart of the user", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+
+            await expect(
+                cartDAO.addProductToCart("customer", "product1")
+            ).resolves.toBe(true);
+        });
+
+        test("It should increment product quantity in the cart of the user", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await expect(
+                cartDAO.addProductToCart("customer", "product1")
+            ).resolves.toBe(true);
+        });
+
+        test("It should throw an error if the product is not in the database", async () => {
+            await cartDAO.createCart("customer");
+            await expect(
+                cartDAO.addProductToCart("customer", "product2")
+            ).rejects.toThrow(ProductNotFoundError);
+        });
+
+        // test("It should throw an error if the product is out of stock", async () => {
+        //     await cartDAO.createCart("customer");
+        //     await ProductDAO.prototype.changeProductQuantity("product1", 0, "2024-06-13");
+        //     console.log(await ProductDAO.prototype.getProducts(null, null, "product1"))
+
+        //     await expect(
+        //         cartDAO.addProductToCart("customer", "product1")
+        //     ).rejects.toThrow(EmptyProductStockError);
+        // });
+
+
+        test("It should throw an error if the quanity of the product in the cart is greater than the stock", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await cartDAO.addProductToCart("customer", "product1");
+
+            await expect(
+                cartDAO.addProductToCart("customer", "product1")
+            ).rejects.toThrow(LowProductStockError);
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the updateCartItem method    *
+     * ********************************************** */
+    describe("Unit test for the updateCartItem method", () => {
+        test("It should update the quantity of the product in the cart", async () => {
+            await cartDAO.createCart("customer");
+            await expect(
+                cartDAO.updateCartItem("customer", 10.0, "product1", 5)
+            ).resolves.toBe(undefined);
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the updateCartTotal method    *
+     * ********************************************** */
+    describe("Unit test for the updateCartTotal method", () => {
+        test("It should update the total of the cart", async () => {
+            await cartDAO.createCart("customer");
+            await expect(cartDAO.updateCartTotal("customer", 30.0)).resolves.toBe(
+                undefined
+            );
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the checkoutCart method    *
+     * ********************************************** */
+    describe("Unit test for the checkoutCart method", () => {
+        test("It should checkout the cart of the user", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+
+            await expect(cartDAO.checkoutCart("customer")).resolves.toBe(true);
+        });
+        test("It should throw an error if the cart is not found", async () => {
+
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new CartNotFoundError()
+            );
+        });
+
+        test("It should throw an error if the cart is empty", async () => {
+            await cartDAO.createCart("customer");
+
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new EmptyCartError()
+            );
+        });
+
+        test("It should throw an error if there are not products in the stock anymore", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.updateCartItem("customer", 10.0, "product1", 2);
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new EmptyProductStockError()
+            );
+        });
+
+        test("It should throw an error if the product is not in the cart", async () => {
+            await cartDAO.createCart("customer");
+
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new ProductNotInCartError()
+            );
+        });
+
+        test("It should throw an error if the product quantity in the cart is greater than the stock", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.updateCartItem("customer", 10.0, "product1", 2);
+
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new LowProductStockError()
+            );
+        });
+
+        test("It should throw an error if the product is not in the cart", async () => {
+            await cartDAO.createCart("customer");
+            await expect(cartDAO.checkoutCart("customer")).rejects.toThrow(
+                new ProductNotInCartError()
+            );
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the removeProductFromCart method    *
+     * ********************************************** */
+    describe("Unit test for the removeProductFromCart method", () => {
+        test("It should remove the product from the cart of the user", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await expect(
+                cartDAO.removeProductFromCart("customer", "product1")
+            ).resolves.toBe(true);
+        });
+
+        test("It should throw an error if the product is not in the database", async () => {
+            await cartDAO.createCart("customer");
+
+            await expect(
+                cartDAO.removeProductFromCart("customer", "product2")
+            ).rejects.toThrow(ProductNotFoundError);
+        });
+
+        test("It should throw an error if the cart is not found", async () => {
+            await expect(
+                cartDAO.removeProductFromCart("customer", "product1")
+            ).rejects.toThrow(CartNotFoundError);
+        });
+
+
+        test("It should throw an error if the cart is empty", async () => {
+            await cartDAO.createCart("customer");
+            await expect(
+                cartDAO.removeProductFromCart("customer", "product1")
+            ).rejects.toThrow(NoCartItemsError);
+        });
+
+        test("It should throw an error if the product is not in the cart", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await ProductDAO.prototype.registerProducts("product2", Category.SMARTPHONE, 2, null, 10.0, "2024-01-01");
+            await expect(
+                cartDAO.removeProductFromCart("customer", "product2")
+            ).rejects.toThrow(ProductNotInCartError);
+        });
+    });
+
+    /* ********************************************** *
+     *    Unit test for the clearCart method    *
+     * ********************************************** */
+    describe("Unit test for the clearCart method", () => {
+        test("It should clear the cart of the user", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await expect(cartDAO.clearCart("customer")).resolves.toBe(true);
+        });
+
+        test("It should throw an error if the cart is not found", async () => {
+            await expect(cartDAO.clearCart("customer")).rejects.toThrow(
+                CartNotFoundError
+            );
         });
 
     });
-
-    /* ****************************************** *
-     * Integration test for the createCart method *    
-     * ****************************************** */
-    test('createCart should create a new cart for a customer', async () => {
-        await dao.createCart('customer1');
-        const carts = await dao.getActiveCartByUserId('customer1');
-        expect(carts).toBeTruthy();
-        expect(carts.customer).toBe('customer1');
-        expect(carts.paid).toBe(false);
-        expect(carts.products.length).toBe(0);
-    });
-
-    /* ************************************************ *
-     * Integration test for the addProductToCart method *    
-     * ************************************************ */
-
-
-
-    /* ***************************************************** *
-     * Integration test for the getActiveCartByUserId method *    
-     * ***************************************************** */
-
-
-
-    /* ************************************************* *
-     * Integration test for the userHasActiveCart method *    
-     * ************************************************* */
-
-
-
     /* ********************************************** *
-     * Integration test for the updateCartItem method *    
+     *    Unit test for the deleteAllCarts method    *
      * ********************************************** */
-
-
-
-    /* *********************************************** *
-     * Integration test for the updateCartTotal method *    
-     * *********************************************** */
-
-
-
-    /* ******************************************** *
-     * Integration test for the checkoutCart method *    
-     * ******************************************** */
-
-    test('checkoutCart should throw EmptyCartError if the cart is empty', async () => {
-        await dao.createCart('customer1');
-        await expect(dao.checkoutCart('customer1')).rejects.toThrow(EmptyCartError);
+    describe("Unit test for the deleteAllCarts method", () => {
+        test("It should delete all carts", async () => {
+            await cartDAO.createCart("customer");
+            await expect(cartDAO.deleteAllCarts()).resolves.toBe(true);
+        });
     });
-
-
-    /* ***************************************************** *
-     * Integration test for the removeProductFromCart method *        //COME GESTIRE L'ERRORE? ProductNotInCartError o ProductNotFoundError
-     * ***************************************************** */
-
-    test('removeProductFromCart should throw ProductNotInCartError if product is not in the cart', async () => {
-        await dao.createCart('customer1');
-        await expect(dao.removeProductFromCart('customer1', 'prod1')).rejects.toThrow(ProductNotInCartError);
-    });
-
-
-    /* ***************************************** *
-     * Integration test for the clearCart method *    
-     * ***************************************** */
-
-
-
     /* ********************************************** *
-     * Integration test for the deleteAllCarts method *    
+     *    Unit test for the getAllCarts method    *
      * ********************************************** */
+    describe("Unit test for the getAllCarts method", () => {
+        test("It should retrieve all carts successfully", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            const expectedCarts = [
+                new Cart("customer", false, null as any, 10.0, [
+                    new ProductInCart("product1", 1, Category.SMARTPHONE, 10.0),
+                ]),
+            ];
 
 
+            await expect(cartDAO.getAllCarts()).resolves.toEqual(expectedCarts);
+        });
+    });
+    /* ********************************************** *
+     *   Unit test for the getCustomerCarts method    *
+     * ********************************************** */
+    describe("Unit test for the getCustomerCarts method", () => {
+        test("It should retrieve all carts of a customer successfully", async () => {
+            await cartDAO.createCart("customer");
+            await cartDAO.addProductToCart("customer", "product1");
+            await cartDAO.checkoutCart("customer");
 
+            const expectedCarts = [
+                new Cart("customer", true, dayjs().format("YYYY-MM-DD"), 10.0, [
+                    new ProductInCart("product1", 1, Category.SMARTPHONE, 10.0),
+                ]),
+            ];
 
-    /* ******************************************* *
-     * Integration test for the getAllCarts method *    
-     * ******************************************* */
-
-
-
-
-    /* ************************************************ *
-     * Integration test for the getCustomerCarts method *    
-     * ************************************************ */
-
-
-
+            await expect(cartDAO.getCustomerCarts("customer")).resolves.toEqual(
+                expectedCarts
+            );
+        });
+    });
 });
